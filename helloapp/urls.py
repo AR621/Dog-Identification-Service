a2c1a61f84ddf5bea8932f99931e4d1757c60400
ar621 <p.dolacinski301@gmail.com>
2024-05-09 22:20:12 +0200

    Reapply "initial connecting of model into webpage"
    This reverts commit 720a12b2141334660347c06599adc2c12f2608f2.

--- helloapp/urls.py
+++ helloapp/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from helloapp import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('about/', views.aboutpage, name='about'),
     path('', views.homepage, name='home'),
+    # path('upload/', views.upload_photo, name='upload_photo'),
+    path('dog/', views.upload_image, name='upload_photo'),
+    path('results/', views.classify_photo, name='classifier'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
--- helloproject/settings.py
+++ helloproject/settings.py
@@ -78,4 +78,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+# configure database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 STATIC_URL = '/static/'
