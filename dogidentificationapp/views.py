a0685c4a59adfc5ed929b41c497e905584923fc5
ar621 <p.dolacinski301@gmail.com>
2024-05-15 17:13:43 +0200

    Changed input image handling to not require saving to db

--- dogidentificationapp/views.py
+++ dogidentificationapp/views.py
@@ -11,14 +11,10 @@ from dogidentificationapp.models import Photo
 
 
 def homepage(request):
-    service = os.environ.get('K_SERVICE', 'Unknown service')
-    revision = os.environ.get('K_REVISION', 'Unknown revision')
+    # service = os.environ.get('K_SERVICE', 'Unknown service')
+    # revision = os.environ.get('K_REVISION', 'Unknown revision')
     
-    return render(request, 'homepage.html', context={
-        "message": "It's running!",
-        "Service": service,
-        "Revision": revision,
-    })
+    return render(request, 'homepage.html')
 
 
 def aboutpage(request):
@@ -26,13 +22,25 @@ def aboutpage(request):
 
 
 def classify_dogs(request):
+    save_image_to_db = False
+
     if request.method == 'POST':
         form = PhotoForm(request.POST, request.FILES)
         if form.is_valid():
             # Save the uploaded image
-            photo_instance = form.save()
+            if save_image_to_db:
+                photo_instance = form.save()
+            else:
+                # photo_instance = Photo(image=form.cleaned_data['image'])
+                photo_instance = form.save(commit=False)
+                # photo_instance.image = form.cleaned_data['image'].read()
+
+
             # Open the uploaded image and convert it to a PIL Image fro classification
-            image = Image.open(io.BytesIO(photo_instance.image))
+            try:
+                image = Image.open(io.BytesIO(photo_instance.image))
+            except IOError:
+                return HttpResponse("Invalid image file.")
             
             # Convert the image to JPG format if it's not already
             if image.format != 'JPEG':
@@ -45,13 +53,20 @@ def classify_dogs(request):
             # change results into percentage and round 2 2 decimal places and change format of title to not include _
             results = [(label.replace('_', ' ').title(), round(confidence * 100, 2)) for label, confidence in results]
 
+            # lines = photo_instance.image.split('\n')
+            image_b64 = base64.b64encode(photo_instance.image).decode('utf-8')
             # Pass the saved photo instance and results to the template context to be rendered
-            return render(request, 'dog_classifier.html', {'form': form, 'img_obj': photo_instance, 'results': results})
+            return render(request, 'dog_classifier.html', {'form': form, 'img_obj': image_b64, 'results': results})
     else:
         form = PhotoForm()
     return render(request, 'dog_classifier.html', {'form': form})
 
 
-def serve_image(request, photo_id):
-    photo = Photo.objects.get(id=photo_id)
-    return HttpResponse(photo.image, content_type="image/jpeg")
+def serve_image(request):
+    try:
+        photo_file = request.FILES['image']
+        photo_file.open()
+        return HttpResponse(photo_file.read(), content_type="image/jpeg")
+    except KeyError:
+        # Handle the case where 'image' key is not found in request.FILES
+        return HttpResponse(status=404)
