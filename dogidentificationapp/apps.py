162544b74cf8c53ca75a996280a721a4be4a1677
ar621 <p.dolacinski301@gmail.com>
2024-05-15 12:12:59 +0200

    moed model loading from views.py to apps.py so that model is initialised at app startup

--- dogidentificationapp/apps.py
+++ dogidentificationapp/apps.py
@@ -1,5 +1,16 @@
 from django.apps import AppConfig
+import os
+from dogidentificationapp.dog_classifier import DogClassifier
 
 
 class ApplicationConfig(AppConfig):
     name = 'dogidentificationapp'
+
+    def ready(self):
+        # define paths for model import
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        model_path = os.path.join(current_dir, 'models', 'model.pth')
+        class_names_path = os.path.join(current_dir, 'models', 'class_names.txt')
+
+        # load the model        
+        self.model = DogClassifier(model_path, class_names_path)
--- dogidentificationapp/views.py
+++ dogidentificationapp/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
 import io
-import os
 import requests
 from django.shortcuts import render, redirect, HttpResponse
+from django.apps import apps
 from .forms import PhotoForm
 import base64
 from PIL import Image
 from dogidentificationapp.models import Photo
-from dogidentificationapp.dog_classifier import DogClassifier
 
 def homepage(request):
     service = os.environ.get('K_SERVICE', 'Unknown service')
@@ -28,20 +27,16 @@ def classify_dogs(request):
         if form.is_valid():
             # Save the uploaded image
             photo_instance = form.save()
-
-            current_dir = os.path.dirname(os.path.abspath(__file__))
-            model_path = os.path.join(current_dir, 'models', 'model.pth')
-            class_names_path = os.path.join(current_dir, 'models', 'class_names.txt')
-
-            model = DogClassifier(model_path, class_names_path)
             # Open the uploaded image and convert it to a PIL Image fro classification
             image = Image.open(io.BytesIO(photo_instance.image))
-
+            
             # Convert the image to JPG format if it's not already
             if image.format != 'JPEG':
                 # If the image is not already in JPEG format, convert it
                 image = image.convert('RGB')
 
+            # Access the loaded model from the app config
+            model = apps.get_app_config('dogidentificationapp').model
             results = model.classify_dog(image)
             # change results into percentage and round 2 2 decimal places and change format of title to not include _
             results = [(label.replace('_', ' ').title(), round(confidence * 100, 2)) for label, confidence in results]
