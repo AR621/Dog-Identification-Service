b6cad1aea0b58d26e6506899b29ecdb043dcf1d8
ar621 <p.dolacinski301@gmail.com>
2024-06-02 14:55:07 +0200

    Fixed result list extending webpage width when being hidden

--- dogidentificationapp/static/results_list.js
+++ dogidentificationapp/static/results_list.js
@@ -1,13 +1,22 @@
 function toggleResults() {
-
     var resultsContainer = document.getElementById('results-container');
     var resultsToggle = document.querySelector('.results-toggle');
+    
+    // Toggle the visibility of the results container
     resultsContainer.classList.toggle('hidden');
+    
+    // Toggle the appearance of the results toggle button
     if (resultsContainer.classList.contains('hidden')) {
         resultsContainer.style.right = '-20em';
         resultsToggle.innerHTML = '&#x25C4;';
+        
+        // Prevent horizontal scrolling when the results list is hidden
+        document.body.style.overflowX = 'hidden';
     } else {
         resultsContainer.style.right = '0';
         resultsToggle.innerHTML = '&#x25BA;';
+        
+        // Allow horizontal scrolling when the results list is visible
+        document.body.style.overflowX = '';
     }
 }
